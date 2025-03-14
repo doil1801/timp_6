69c272d4134997f9bfe7c7612a69571ec9d59e3d
doil <doil05@mail.ru>
2025-03-14 09:22:02 +0300

    No html

--- main.py
+++ main.py
@@ -4,19 +4,12 @@ import hashlib
 
 app = FastAPI()
 
-@app.get("/", response_class=HTMLResponse)
+@app.get("/")
 def read_root():
-    return """
-    <html>
-        <body>
-            <h3>Для того, чтобы хэшировать строку введите следующий запрос /hash/algorithm/string,
-                Поддерживаемые алгоритмы: md5, sha1, sha256, sha512.
-            </h3>
-        </body>
-    </html>
-    """
+    return { 'info': """Для того, чтобы хэшировать строку введите следующий запрос /hash/algorithm/string. Поддерживаемые алгоритмы: md5, sha1, sha256, sha512."""
+            }
 
-@app.get("/hash/{algorithm}/{input_string}", response_class=HTMLResponse)
+@app.get("/hash/{algorithm}/{input_string}")
 def read_root(algorithm, input_string):
     if algorithm == 'md5':
         result = hashlib.md5(input_string.encode('utf-8'))
@@ -27,25 +20,11 @@ def read_root(algorithm, input_string):
     elif algorithm == 'sha512':
         result = hashlib.sha512(input_string.encode('utf-8'))
     else:
-        return """
-        <html>
-            <body>
-                <h3>Неподдерживаемый вид хэширования</h3>
-            </body>
-        </html>
-        """
-    return f"""
-    <html>
-        <body>
-            <h3>
-            Исходная строка: {input_string}
-            </h3>
-            <h3>
-            Вид хэширования: {algorithm}
-            </h3>
-            <h3>
-            Ваш результат: {result.hexdigest()}
-            </h3>
-        </body>
-    </html>
-    """
+        return {
+            "algorithm": "Неподдерживаемый алгоритм",
+            "result_string": "Неподдерживаемый алгоритм"
+        }
+    return {
+            "algorithm": algorithm,
+            "result_string": result.hexdigest()
+        }
